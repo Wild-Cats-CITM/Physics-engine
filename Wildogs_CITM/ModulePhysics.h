044ace44fedcb2097274096a24d7347ef172ed0e
christianpi4 <chricraft4100@gmail.com>
2019-11-10 20:18:42 +0100

    Added Base

---
+++ Wildogs_CITM/ModulePhysics.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+
+
+#define GRAVITY_X 0.0f
+#define GRAVITY_Y -7.0f
+
+#define PIXELS_PER_METER 50.0f // if touched change METER_PER_PIXEL too
+#define METER_PER_PIXEL 0.02f // this is 1 / PIXELS_PER_METER !
+
+#define METERS_TO_PIXELS(m) ((int) floor(PIXELS_PER_METER * m))
+#define PIXEL_TO_METERS(p)  ((float) METER_PER_PIXEL * p)
+
+// Small class to return to other modules to track position and rotation of physics bodies
+
+
+// Module --------------------------------------
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+
+	
+
+private:
+
+	bool debug = false;
+	
+	
+};
---
+++ Wildogs_CITM/ModuleSceneIntro.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Module.h"
+#include "p2List.h"
+#include "Globals.h"
+
+class PhysBody;
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+
+	SDL_Texture* circle;
+	SDL_Texture* box;
+
+};
